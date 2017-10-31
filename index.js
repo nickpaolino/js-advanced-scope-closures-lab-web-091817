f170f11552ae4da02731760b499584aea907f3b1
Nick Paolino <github email address>
2017-10-30 21:05:55 -0400

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function produceTipCalculator(tipPercentage){
     return amount * tipPercentage;
   }
 }
+
+function createDriver(){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+}
